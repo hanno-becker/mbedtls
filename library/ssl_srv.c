e6e835a40e5d6a350aa5a9c44c8d500bfbb1ba0e
Hanno Becker <hanno.becker@arm.com>
2017-04-27 10:25:43 +0100

    Choose ciphersuite only if suitable hash algorithm is present.
    The previous commit made available a table of suitable hash algorithms
    for each signature algorithm. This commit modifies the ciphersuite
    checking function `ssl_ciphersuite_match` to refuse a suite if there
    is no suitable hash algorithm.

--- library/ssl_srv.c
+++ library/ssl_srv.c
@@ -749,6 +749,11 @@ static int ssl_ciphersuite_match( mbedtls_ssl_context *ssl, int suite_id,
 {
     const mbedtls_ssl_ciphersuite_t *suite_info;
 
+#if defined(MBEDTLS_SSL_PROTO_TLS1_2) && \
+    defined(MBEDTLS_KEY_EXCHANGE__WITH_CERT__ENABLED)    
+    mbedtls_sig_type_t sig_type;
+#endif
+
     suite_info = mbedtls_ssl_ciphersuite_from_id( suite_id );
     if( suite_info == NULL )
     {
@@ -814,6 +819,21 @@ static int ssl_ciphersuite_match( mbedtls_ssl_context *ssl, int suite_id,
         return( 0 );
     }
 #endif
+
+#if defined(MBEDTLS_SSL_PROTO_TLS1_2) && \
+    defined(MBEDTLS_KEY_EXCHANGE__WITH_CERT__ENABLED)
+    /* If the ciphersuite requires signing, check whether
+     * a suitable hash algorithm is present. */
+    if( ssl->minor_ver == MBEDTLS_SSL_MINOR_VERSION_3                                        &&
+        ( sig_type = mbedtls_ssl_get_ciphersuite_sig_alg( suite_info ) ) != MBEDTLS_SIG_NONE &&
+        ssl->handshake->hash_alg[sig_type] == MBEDTLS_MD_NONE )
+    {
+        MBEDTLS_SSL_DEBUG_MSG( 3, ( "ciphersuite mismatch: no suitable hash algorithm "
+                                    "for signature algorithm %d", sig_type ) );
+        return( 0 );
+    }
+#endif /* MBEDTLS_SSL_PROTO_TLS1_2 &&
+          MBEDTLS_KEY_EXCHANGE__WITH_CERT__ENABLED */
 
 #if defined(MBEDTLS_X509_CRT_PARSE_C)
     /*
