7d511788739e59f6d33bec187d291aa67e54dfd2
Hanno Becker <hanno.becker@arm.com>
2017-07-24 15:06:17 +0100

    Avoid downcast of error code to bool in ECDSA key generation

--- library/ecdsa.c
+++ library/ecdsa.c
@@ -411,8 +411,13 @@ cleanup:
 int mbedtls_ecdsa_genkey( mbedtls_ecdsa_context *ctx, mbedtls_ecp_group_id gid,
                   int (*f_rng)(void *, unsigned char *, size_t), void *p_rng )
 {
-    return( mbedtls_ecp_group_load( &ctx->grp, gid ) ||
-            mbedtls_ecp_gen_keypair( &ctx->grp, &ctx->d, &ctx->Q, f_rng, p_rng ) );
+    int ret;
+    if( ( ret = mbedtls_ecp_group_load( &ctx->grp, gid ) ) != 0 ||
+        ( ret = mbedtls_ecp_gen_keypair( &ctx->grp, &ctx->d, &ctx->Q,
+                                         f_rng, p_rng ) )  != 0 )
+        return( ret );
+
+    return( 0 );
 }
 
 /*
