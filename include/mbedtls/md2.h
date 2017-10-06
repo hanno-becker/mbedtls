54d32641442e53e0e19ffd27a8af1efd9eb1233c
Hanno Becker <hanno.becker@arm.com>
2017-10-06 07:17:28 +0100

    Style corrections in md.h, md2.h, arc4.h

--- include/mbedtls/md2.h
+++ include/mbedtls/md2.h
@@ -149,7 +149,9 @@ void mbedtls_md2_starts( mbedtls_md2_context *ctx );
  *                 in a future version of the library.
  *
  */
-void mbedtls_md2_update( mbedtls_md2_context *ctx, const unsigned char *input, size_t ilen );
+void mbedtls_md2_update( mbedtls_md2_context *ctx,
+                         const unsigned char *input,
+                         size_t ilen );
 
 /**
  * \brief          MD2 final digest
