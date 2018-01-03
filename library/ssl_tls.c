4e79604e3b83be9d9dbc9fdd5aacf3c1f77d4f7e
Hanno Becker <hanno.becker@arm.com>
2018-01-03 15:24:20 +0000

    Correct space needed for MAC in case of NULL cipher
    The macro constant `MBEDTLS_SSL_MAC_ADD` defined in `ssl_internal.h`
    defines an upper bound for the amount of space needed for the record
    authentication tag. Its definition distinguishes between the
    presence of an ARC4 or CBC ciphersuite suite, in which case the maximum
    size of an enabled SHA digest is used; otherwise, `MBEDTLS_SSL_MAC_ADD`
    is set to 16 to accomodate AEAD authentication tags.
    
    This assignment has a flaw in the situation where confidentiality is
    not needed and the NULL cipher is in use. In this case, the
    authentication tag also uses a SHA digest, but the definition of
    `MBEDTLS_SSL_MAC_ADD` doesn't guarantee enough space.
    
    The present commit fixes this by distinguishing between the presence
    of *some* ciphersuite using a MAC, including those using a NULL cipher.
    For that, the previously internal macro `SSL_SOME_MODES_USE_MAC` from
    `ssl_tls.c` is renamed and moved to the public macro
    `MBEDTLS_SOME_MODES_USE_MAC` defined in `ssl_internal.h`.

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -1274,12 +1274,6 @@ static void ssl_mac( mbedtls_md_context_t *md_ctx,
 }
 #endif /* MBEDTLS_SSL_PROTO_SSL3 */
 
-#if defined(MBEDTLS_ARC4_C) || defined(MBEDTLS_CIPHER_NULL_CIPHER) ||     \
-    ( defined(MBEDTLS_CIPHER_MODE_CBC) &&                                  \
-      ( defined(MBEDTLS_AES_C) || defined(MBEDTLS_CAMELLIA_C) ) )
-#define SSL_SOME_MODES_USE_MAC
-#endif
-
 /*
  * Encryption/decryption functions
  */
@@ -1312,7 +1306,7 @@ static int ssl_encrypt_buf( mbedtls_ssl_context *ssl )
     /*
      * Add MAC before if needed
      */
-#if defined(SSL_SOME_MODES_USE_MAC)
+#if defined(MBEDTLS_SSL_SOME_MODES_USE_MAC)
     if( mode == MBEDTLS_MODE_STREAM ||
         ( mode == MBEDTLS_MODE_CBC
 #if defined(MBEDTLS_SSL_ENCRYPT_THEN_MAC)
@@ -1365,7 +1359,7 @@ static int ssl_encrypt_buf( mbedtls_ssl_context *ssl )
         ssl->out_msglen += ssl->transform_out->maclen;
         auth_done++;
     }
-#endif /* AEAD not the only option */
+#endif /* MBEDTLS_SSL_SOME_MODES_USE_MAC */
 
     /*
      * Encrypt
@@ -1620,7 +1614,7 @@ static int ssl_decrypt_buf( mbedtls_ssl_context *ssl )
     size_t i;
     mbedtls_cipher_mode_t mode;
     int auth_done = 0;
-#if defined(SSL_SOME_MODES_USE_MAC)
+#if defined(MBEDTLS_SSL_SOME_MODES_USE_MAC)
     size_t padlen = 0, correct = 1;
 #endif
 
@@ -1967,7 +1961,7 @@ static int ssl_decrypt_buf( mbedtls_ssl_context *ssl )
      * Authenticate if not done yet.
      * Compute the MAC regardless of the padding result (RFC4346, CBCTIME).
      */
-#if defined(SSL_SOME_MODES_USE_MAC)
+#if defined(MBEDTLS_SSL_SOME_MODES_USE_MAC)
     if( auth_done == 0 )
     {
         unsigned char mac_expect[MBEDTLS_SSL_MAC_ADD];
@@ -2051,7 +2045,7 @@ static int ssl_decrypt_buf( mbedtls_ssl_context *ssl )
         if( correct == 0 )
             return( MBEDTLS_ERR_SSL_INVALID_MAC );
     }
-#endif /* SSL_SOME_MODES_USE_MAC */
+#endif /* MBEDTLS_SSL_SOME_MODES_USE_MAC */
 
     /* Make extra sure authentication was performed, exactly once */
     if( auth_done != 1 )
