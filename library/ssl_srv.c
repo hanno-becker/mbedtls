b0bdd40f8dbd03587efb522ca4d27a4c36beecb9
Hanno Becker <hanno.becker@arm.com>
2018-05-21 17:16:42 +0100

    Restructure incoming CliKeyExch: Add frame for restructuring
    This commit adds declarations and dummy implementations for
    the restructured incoming client key exchange handling that
    will replace the previous ssl_parse_client_key_exchange().
    
    The entry point for the CliKeyExchange handling that is called
    from the handshake state machine is
    
       `ssl_process_client_key_exchange()`,
    
    splitting the processing into the following steps:
    
    - Fetching: Read next message from the messaging layer
                and check that it has the correct type.
                The ClientKeyExchange message is never
                omitted, so there is no ambiguity in what
                to expect, and hence no dedicated preparation
                step as for other handshake states.
    - Parsing:  Parse the ClientKeyExchange message and
                use the information in it to derive keying
                material such as the shared (EC)DHE secret.
    - Postprocessing:
                Compute the session keys from the available
                keying material. This splits in two steps:
                (1) Build the PreMasterSecret (PMS) from the
                    available keying material, e.g. concatenate
                    the (EC)DHE secret with a PSK, if used.
                (2) Extract the MasterSecret and Session Keys
                    from the PreMasterSecret.
    
    The subsequent commits will scatter the code from the previous
    monolithic function ssl_parse_client_key_exchange() among those
    dedicated functions, commenting out each part of
    ssl_parse_client_key_exchange() that has already been dealt with.
    This gradual progression is meant to ease reviewing. Once all
    code has been moved and all changes explained,
    ssl_parse_client_key_exchange() will be removed.

--- library/ssl_srv.c
+++ library/ssl_srv.c
@@ -4026,15 +4026,43 @@ static int ssl_parse_client_psk_identity( mbedtls_ssl_context *ssl, unsigned cha
 }
 #endif /* MBEDTLS_KEY_EXCHANGE__SOME__PSK_ENABLED */
 
-static int ssl_parse_client_key_exchange( mbedtls_ssl_context *ssl )
+/*
+ *
+ * STATE HANDLING: Client Key Exchange
+ *
+ */
+
+/*
+ * Overview
+ */
+
+/* Main entry point; orchestrates the other functions. */
+static int ssl_process_client_key_exchange( mbedtls_ssl_context *ssl );
+
+static int ssl_client_key_exchange_parse( mbedtls_ssl_context *ssl,
+                                          unsigned char *buf,
+                                          size_t buflen );
+/* Update the handshake state */
+static int ssl_client_key_exchange_postprocess( mbedtls_ssl_context *ssl );
+
+/*
+ * Implementation
+ */
+
+static int ssl_process_client_key_exchange( mbedtls_ssl_context *ssl )
 {
     int ret;
-    const mbedtls_ssl_ciphersuite_t *ciphersuite_info;
-    unsigned char *p, *end;
 
-    ciphersuite_info = ssl->transform_negotiate->ciphersuite_info;
+#if defined(MBEDTLS_SSL_ASYNC_PRIVATE) && \
+    ( defined(MBEDTLS_KEY_EXCHANGE_RSA_ENABLED) || \
+      defined(MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED) )
+    const mbedtls_ssl_ciphersuite_t *ciphersuite_info =
+        ssl->transform_negotiate->ciphersuite_info;
+#endif
 
-    MBEDTLS_SSL_DEBUG_MSG( 2, ( "=> parse client key exchange" ) );
+    MBEDTLS_SSL_DEBUG_MSG( 2, ( "=> process client key exchange" ) );
+
+    /* The ClientKeyExchange message is never skipped. */
 
 #if defined(MBEDTLS_SSL_ASYNC_PRIVATE) && \
     ( defined(MBEDTLS_KEY_EXCHANGE_RSA_ENABLED) || \
@@ -4050,6 +4078,90 @@ static int ssl_parse_client_key_exchange( mbedtls_ssl_context *ssl )
     }
     else
 #endif
+    {
+        /* Reading step */
+        if( ( ret = mbedtls_ssl_read_record( ssl, 1 ) ) != 0 )
+        {
+            MBEDTLS_SSL_DEBUG_RET( 1, "mbedtls_ssl_read_record", ret );
+            return( ret );
+        }
+
+        if( ssl->in_msgtype != MBEDTLS_SSL_MSG_HANDSHAKE ||
+            ssl->in_msg[0]  != MBEDTLS_SSL_HS_CLIENT_KEY_EXCHANGE )
+        {
+            MBEDTLS_SSL_DEBUG_MSG( 1, ( "bad client key exchange message" ) );
+            ssl->send_alert = MBEDTLS_SSL_ALERT_LEVEL_FATAL;
+            ssl->alert_type = MBEDTLS_SSL_ALERT_MSG_UNEXPECTED_MESSAGE;
+            ret = MBEDTLS_ERR_SSL_UNEXPECTED_MESSAGE;
+            goto cleanup;
+        }
+    }
+
+    SSL_PROC_CHK( ssl_client_key_exchange_parse( ssl, ssl->in_msg,
+                                                 ssl->in_hslen ) );
+
+    /* Derive keys and update state */
+    SSL_PROC_CHK( ssl_client_key_exchange_postprocess( ssl ) );
+
+cleanup:
+
+    /* Ignore error code for now */
+    /* QUESTION: Should we default to INTERNAL_ERROR if no error code
+     *           was set from the low-level functions? */
+    mbedtls_ssl_handle_pending_alert( ssl );
+
+    MBEDTLS_SSL_DEBUG_MSG( 2, ( "<= process client key exchange" ) );
+    return( ret );
+}
+
+static int ssl_client_key_exchange_parse( mbedtls_ssl_context *ssl,
+                                          unsigned char *buf,
+                                          size_t buflen )
+{
+    /* TBD */
+}
+
+/* Update the handshake state */
+static int ssl_client_key_exchange_postprocess( mbedtls_ssl_context *ssl )
+{
+    /* TODO: Derive keys */
+
+    /* Update state */
+    ssl->state = MBEDTLS_SSL_CERTIFICATE_VERIFY;
+    return( 0 );
+}
+
+/* OLD CODE
+ *
+ * Temporarily included to gradually move it to the correct
+ * place in the restructured code.
+ *
+ */
+
+static int ssl_parse_client_key_exchange( mbedtls_ssl_context *ssl )
+{
+    int ret;
+    const mbedtls_ssl_ciphersuite_t *ciphersuite_info;
+    unsigned char *p, *end;
+
+    ciphersuite_info = ssl->transform_negotiate->ciphersuite_info;
+
+    MBEDTLS_SSL_DEBUG_MSG( 2, ( "=> parse client key exchange" ) );
+
+/* #if defined(MBEDTLS_SSL_ASYNC_PRIVATE) && \ */
+/*     ( defined(MBEDTLS_KEY_EXCHANGE_RSA_ENABLED) || \ */
+/*       defined(MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED) ) */
+/*     if( ( ciphersuite_info->key_exchange == MBEDTLS_KEY_EXCHANGE_RSA_PSK || */
+/*           ciphersuite_info->key_exchange == MBEDTLS_KEY_EXCHANGE_RSA ) && */
+/*         ( ssl->handshake->async_in_progress != 0 ) ) */
+/*     { */
+/*         /\* We've already read a record and there is an asynchronous */
+/*          * operation in progress to decrypt it. So skip reading the */
+/*          * record. *\/ */
+/*         MBEDTLS_SSL_DEBUG_MSG( 3, ( "will resume decryption of previously-read record" ) ); */
+/*     } */
+/*     else */
+/* #endif */
     if( ( ret = mbedtls_ssl_read_record( ssl, 1 ) ) != 0 )
     {
         MBEDTLS_SSL_DEBUG_RET( 1, "mbedtls_ssl_read_record", ret );
@@ -4302,6 +4414,8 @@ static int ssl_parse_client_key_exchange( mbedtls_ssl_context *ssl )
 
     return( 0 );
 }
+
+/* END OF OLD CODE */
 
 /*
  *
@@ -4724,7 +4838,7 @@ int mbedtls_ssl_handshake_server_step( mbedtls_ssl_context *ssl )
             break;
 
         case MBEDTLS_SSL_CLIENT_KEY_EXCHANGE:
-            ret = ssl_parse_client_key_exchange( ssl );
+            ret = ssl_process_client_key_exchange( ssl );
             break;
 
         case MBEDTLS_SSL_CERTIFICATE_VERIFY:
