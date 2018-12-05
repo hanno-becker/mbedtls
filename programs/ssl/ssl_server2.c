a03472abb72158e9787e7a97d0ffdbb4754186cc
Hanno Becker <hanno.becker@arm.com>
2018-12-05 16:49:42 +0000

    Correct white spaces in ssl_server2 and ssl_client2

--- programs/ssl/ssl_server2.c
+++ programs/ssl/ssl_server2.c
@@ -1999,7 +1999,7 @@ int main( int argc, char *argv[] )
 #else
     {
         ret = 1;
-        mbedtls_printf("MBEDTLS_CERTS_C not defined.");
+        mbedtls_printf( "MBEDTLS_CERTS_C not defined." );
     }
 #endif
     if( ret < 0 )
